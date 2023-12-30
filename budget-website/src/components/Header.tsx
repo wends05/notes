b5e150bf46ea133f6433faa0d188b99014009a50
wends05 <143113418+wends05@users.noreply.github.com>
2023-12-30 08:34:06 +0000

    hala nag damo

---
+++ budget-website/src/components/Header.tsx
@@ -0,0 +1,18 @@
+
+
+const Header = () => {
+  return (
+    <ul className={"flex flex-row justify-center gap-4 h-10 mt-2"}>
+        <li>
+          <a href="">
+            Home
+          </a>
+        </li>
+        <li>
+            Calendar
+        </li>
+    </ul>
+  )
+}
+
+export default Header
---
+++ tensor-test/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import * as tf from "@tensorflow/tfjs"
+import * as handpose from "@tensorflow-models/handpose"
+import Webcam from 'react-webcam'
+
+
+function App() {
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
