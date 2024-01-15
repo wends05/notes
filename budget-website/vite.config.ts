e80281f62652b37f4822c002d3ed28db2ffbbcbe
Wendell Dador <cxnner05@gmail.com>
2024-01-15 08:19:50 +0800

    weee

--- budget-website/vite.config.ts
+++ budget-website/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/capycopy",
+  base: "/capycopy/",
   plugins: [react()],
   resolve: {
     alias: {
